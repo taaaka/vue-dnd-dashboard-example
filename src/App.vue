9100aade4385277c55e3ce654e0a50665ce88d28
taaaka <Rw.taaaka@gmail.com>
2021-04-08 22:48:38 +0900

    add vue-draggable&example

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,60 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <hr>
+  <draggable 
+    v-model="draggableItems" 
+    group="people" 
+    item-key="id"
+    @end="handleDragEnd"
+  >
+    <template #item="{element}">
+      <div>value is : {{element.value}}</div>
+    </template>
+  </draggable>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, isRef, reactive, ref, toRaw, unref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+
+import draggable from 'vuedraggable'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    draggable,
+  },
+  setup() {
+    const draggableItems = ref([
+        {
+          id: 1,
+          value: 'aaa',
+        },
+        {
+          id: 2,
+          value: 'vvv',
+        },
+        {
+          id: 3,
+          value: 'ccc',
+        },
+        {
+          id: 4,
+          value: 'aaasdfasdfa',
+        },
+      ]);
+
+    const handleDragEnd = () => {
+      
+      console.log(toRaw(draggableItems.value).map(v => toRaw(v)));
+    };
+
+    return {
+      draggableItems,
+      handleDragEnd,
+    }
   }
 })
 </script>
