b00d9d81621010f87be4afcc4dbddf166fa5b072
taaaka <Rw.taaaka@gmail.com>
2021-04-12 23:34:12 +0900

    (WIP) dynamic component

---
+++ src/components/DynamicComp1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Dynamic Comp: {{title}}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, isRef, reactive, ref, toRaw, unref } from 'vue'
+
+export default defineComponent({
+  name: 'DynamicComp1',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
